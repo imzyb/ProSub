c25524b461452102209ed93f5ec14e43052a47f1
imzyb <vanness.x@gmail.com>
2025-06-22 00:00:28 +0800

    优化UI

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import './assets/main.css'
+// src/main.js
+import './assets/main.css' // 确保这一行存在
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
